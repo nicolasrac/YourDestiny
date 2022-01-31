2f306ef3304ce3cd0459c7adec398eae8501f02b
Nicolás <95367618+nicolasrac@users.noreply.github.com>
2022-01-31 09:06:00 -0500

    Add files via upload

---
+++ Opcion.py
@@ -0,0 +1,5 @@
+class Opcion:
+    def __init__(self, descripcion, siguienteFragmento):
+        self.descripcion = descripcion
+        self.siguienteFragmento = siguienteFragmento
+
---
+++ Personaje.py
@@ -0,0 +1,7 @@
+class Personaje:
+    def __init__(self):
+        self.supervivencia = 0
+
+    def obtenerEstado(self):
+        return 'Supervivencia: ' + str(self.supervivencia)
+
